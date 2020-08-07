23e43651d1e96dbd339c323ed193a183e1ec5c88
Diego Orellana <diego@admetricks.com>
2020-08-07 15:19:50 -0400

    Fix check and create tables with is_clear flag

--- tests/contrib/backends/sqlalchemy/test_domain_metadata.py
+++ tests/contrib/backends/sqlalchemy/test_domain_metadata.py
@@ -1,10 +1,23 @@
-from frontera.contrib.backends.sqlalchemy.components import DomainMetadata, DomainMetadataKV
+import pytest
+from frontera.contrib.backends.sqlalchemy.components import DomainMetadata, DomainMetadataKV, States
+from frontera.contrib.backends.sqlalchemy.models import StateModel
+from frontera.contrib.backends.sqlalchemy import Distributed
+from frontera.core.models import Request
+from tests.mocks.frontier_manager import FakeFrontierManager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from unittest import TestCase
 import random
 import string
 
+r1 = Request(
+    'https://www.example.com',
+    meta={
+        b'fingerprint': b'10',
+        b'domain': {b'name': b'www.example.com', b'fingerprint': b'81'},
+        b'state': 1
+    }
+)
 
 def random_string(N):
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
@@ -39,4 +52,27 @@ class TestSqlAlchemyDomainMetadata(TestCase):
             assert "key%d" % i in dm
             assert len(dm["key%d" % i]) == 10
             del dm["key%d" % i]
+
+@pytest.fixture(scope="module")
+def sql_backend():
+    manager = FakeFrontierManager.from_settings()
+    backend = Distributed(manager)
+    backend._init_strategy_worker(manager)
+    yield backend
+    backend.frontier_stop()
+    backend.engine.dispose()
+
+def test_check_and_create_tables_is_clear(sql_backend):
+    session_cls = sessionmaker()
+    session_cls.configure(bind=sql_backend.engine)
+    another_session = session_cls()
+
+    sql_backend.states.update_cache([r1])
+    sql_backend.states.flush()
+    model_states = sql_backend.models['StateModel']
+    model_dm = sql_backend.models['DomainMetadataModel']
+    assert another_session.query(model_states).count() == 1
+
+    sql_backend.check_and_create_tables(False, True, (model_states, model_dm))
+    assert another_session.query(model_states).count() == 0
 
